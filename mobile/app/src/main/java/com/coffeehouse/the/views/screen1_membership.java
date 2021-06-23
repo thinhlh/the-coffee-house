1174007005fbcef1bfd110170f8e67603f6e745f
BuiThienNhan-nb <81063048+BuiThienNhan-nb@users.noreply.github.com>
2021-06-23 18:54:41 +0700

    feat: binding membership

--- mobile/app/src/main/java/com/coffeehouse/the/views/screen1_membership.java
+++ mobile/app/src/main/java/com/coffeehouse/the/views/screen1_membership.java
@@ -7,13 +7,17 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 
 import com.coffeehouse.the.R;
+import com.coffeehouse.the.databinding.FragmentScreen1MembershipBinding;
+import com.coffeehouse.the.services.UserRepo;
 
 import org.jetbrains.annotations.NotNull;
 
 public class screen1_membership extends Fragment {
+    private FragmentScreen1MembershipBinding binding;
     public screen1_membership() {
     }
 
@@ -21,7 +25,11 @@ public class screen1_membership extends Fragment {
     @org.jetbrains.annotations.Nullable
     @Override
     public View onCreateView(@NonNull @NotNull LayoutInflater inflater, @Nullable @org.jetbrains.annotations.Nullable ViewGroup container, @Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_screen1__membership,container,false);
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_screen1__membership, container, false);
+
+        binding.setUser(UserRepo.user);
+
+        return binding.getRoot();
     }
 
     @Override

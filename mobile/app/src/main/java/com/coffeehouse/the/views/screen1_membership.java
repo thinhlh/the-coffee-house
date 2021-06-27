22421a36a0bd7342eba4bd57580e5c09a95c1e05
BuiThienNhan-nb <81063048+BuiThienNhan-nb@users.noreply.github.com>
2021-06-27 20:55:46 +0700

    feat: draw barcode for user Id

--- mobile/app/src/main/java/com/coffeehouse/the/views/screen1_membership.java
+++ mobile/app/src/main/java/com/coffeehouse/the/views/screen1_membership.java
@@ -1,9 +1,13 @@
 package com.coffeehouse.the.views;
 
+import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -20,8 +24,17 @@ import com.coffeehouse.the.databinding.FragmentScreen1MembershipBinding;
 import com.coffeehouse.the.databinding.MembershipFragmentBinding;
 import com.coffeehouse.the.services.repositories.UserRepo;
 import com.coffeehouse.the.viewModels.PromotionViewModel;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.EncodeHintType;
+import com.google.zxing.Writer;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.oned.Code128Writer;
+import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
 
 import org.jetbrains.annotations.NotNull;
+
+import java.util.Hashtable;
 
 public class screen1_membership extends Fragment {
     private FragmentScreen1MembershipBinding binding;
@@ -54,8 +67,33 @@ public class screen1_membership extends Fragment {
             bottomSheet.show(getFragmentManager(), "Promotion Detail");
         });
 
+        createUserBarCode();
+        binding.userId.setText(FirebaseAuth.getInstance().getUid());
+
         return binding.getRoot();
     }
+
+    private void createUserBarCode() {
+        try {
+            Hashtable<EncodeHintType, ErrorCorrectionLevel> hintMap = new Hashtable<EncodeHintType, ErrorCorrectionLevel>();
+            hintMap.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.L);
+            Writer codeWriter;
+            codeWriter = new Code128Writer();
+            BitMatrix byteMatrix = codeWriter.encode(FirebaseAuth.getInstance().getUid(), BarcodeFormat.CODE_128, 400, 220, hintMap);
+            int width = byteMatrix.getWidth();
+            int height = byteMatrix.getHeight();
+            Bitmap bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+            for (int i = 0; i < width; i++) {
+                for (int j = 0; j < height; j++) {
+                    bitmap.setPixel(i, j, byteMatrix.get(i, j) ? Color.BLACK : Color.WHITE);
+                }
+            }
+            binding.userBarcode.setImageBitmap(bitmap);
+        } catch (Exception e) {
+            Log.d("BARCODE", "Create bar code failed cause by" + e);
+        }
+    }
+
 
     private void getPromotions(PromotionAdapter promotionAdapter) {
         promotionViewModel.getPromotions().observe(getViewLifecycleOwner(), promotionAdapter::setItems);
